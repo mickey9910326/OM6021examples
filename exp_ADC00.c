440dd2e7495d4f7a3fd870d4b8a96e36ef89679b
mickey9910326 <mickey9910326@gmail.com>
2017-12-21 15:26:00 +0800

    add example of DAC00

---
+++ exp_ADC00.c
@@ -0,0 +1,22 @@
+#include "ASA_Lib.h"
+
+int main() {
+    ASA_M128_set();
+    printf("ASA_DAC example code. by LiYu 2017.12.21\n");
+
+    char ID_DAC = 1;
+    int data = 20;
+
+	ASA_DAC00_set(1, 200, 0x80, 7, 0x01); // 單通道非同步模式
+	ASA_DAC00_set(1, 200, 0x40, 6, 0x00); // 同步輸出0
+	ASA_DAC00_set(1, 200, 0x30, 4, 0x00); // 輸出通道0 S1S2
+
+    while (1) {
+        printf("input data:");
+        scanf("%d", &data);
+
+        ASA_DAC00_put(ID_DAC, 0, 2, &data);			//DAC卡輸出波型
+    }
+
+    return 0;
+}
