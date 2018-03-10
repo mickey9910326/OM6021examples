e4ade166cb5bd9cecab7d0613ea3ba4a4a0faeed
mickey9910326 <mickey9910326@gmail.com>
2018-03-10 20:59:05 +0800

    update exp2_dac00

---
+++ exp2_dac00.c
@@ -0,0 +1,31 @@
+/**
+ * @file exp2_dac00.c
+ * @author LiYu
+ * @date 2018.03.10
+ * @brief Example of using ADC00 to output an assigned volt.
+ *
+ */
+
+#include "ASA_Lib.h"
+#include "lib/DAC00/ASA_Lib_DAC00.h"
+
+int main() {
+    ASA_M128_set();
+    printf("ASA_DAC example code. by LiYu 2017.12.21\n");
+
+    char ID_DAC = 1;
+    int data = 20;
+
+	ASA_DAC00_set(ID_DAC, 200, 0x80, 7, 0x01); // 單通道非同步模式
+	ASA_DAC00_set(ID_DAC, 200, 0x40, 6, 0x00); // 同步輸出1
+	ASA_DAC00_set(ID_DAC, 200, 0x30, 4, 0x00); // 輸出通道1 S1S2
+
+    while (1) {
+        printf("input data:");
+        scanf("%d", &data);
+
+        ASA_DAC00_put(ID_DAC, 0, 2, &data); //DAC卡輸出波型
+    }
+
+    return 0;
+}
